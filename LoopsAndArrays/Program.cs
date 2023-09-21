92104cf929647b038773f681cbe67cabc943d26d
Nikoloz Papaskiri <nkpapaskiri@gmail.com>
2023-09-21 22:50:31 +0400

    array examples

--- LoopsAndArrays/Program.cs
+++ LoopsAndArrays/Program.cs
@@ -1,65 +1,81 @@
-﻿////string[] myStringArray = new string[] { "gio", "lasha","saba", "misho"};
+﻿//////string[] myStringArray = new string[] { "gio", "lasha","saba", "misho"};
 
-////int[] myIntArray =  new int[4];
+//////int[] myIntArray =  new int[4];
 
-////Console.WriteLine(myStringArray[5]);
+//////Console.WriteLine(myStringArray[5]);
 
 
-//string[] genres = new string[] { "Scifi", "Comedy", "Adventurious", "Educational" };
-//int[] counts = new int[4];
+////string[] genres = new string[] { "Scifi", "Comedy", "Adventurious", "Educational" };
+////int[] counts = new int[4];
 
-//int iteration = 5;
+////int iteration = 5;
 
-//while (iteration > 0)
+////while (iteration > 0)
+////{
+////    Console.WriteLine("Add movie: (0 - Scifi, 1 - Comedy, 2 - Adventurious, 3 - Educational)");
+////    string input = Console.ReadLine();
+////    int index = int.Parse(input);
+
+////    counts[index]++;
+
+////    Console.WriteLine($"{genres[index]} count: {counts[index]}");
+
+////    iteration--;
+////}
+
+////foreach (string item in genres) 
+////    { 
+////        Console.WriteLine(item); 
+////    }
+
+////int[,] myIntArray = new int[] { { 1, 2 }, { 3, 4,3 } };  //orive masivi ertidaigive zomis unda iyos tore ichxubebs tu ar agwer sigrdzes
+////int[,] myIntArray = new int[2, 2] { { 1, 2 }, { 3, 4 } };
+////for (int i = 0; i < 2; i++)
+////{
+////    for (int j = 0; j < 2; j++)
+////    {
+////        if (j % 2 == 1)
+////            Console.WriteLine(myIntArray[i, j]);
+////        else
+////            Console.Write(myIntArray[i, j] + " ");
+////    }
+////}
+
+
+//int[,] my2dIntArray = new int[2, 3];
+
+//int lengthOfFirstDimension = my2dIntArray.GetLength(0);
+//int lengthOfSecondDimension = my2dIntArray.GetLength(1);
+
+//for (int i = 0; i < lengthOfFirstDimension; i++)
 //{
-//    Console.WriteLine("Add movie: (0 - Scifi, 1 - Comedy, 2 - Adventurious, 3 - Educational)");
-//    string input = Console.ReadLine();
-//    int index = int.Parse(input);
-
-//    counts[index]++;
-
-//    Console.WriteLine($"{genres[index]} count: {counts[index]}");
-
-//    iteration--;
-//}
-
-//foreach (string item in genres) 
-//    { 
-//        Console.WriteLine(item); 
-//    }
-
-//int[,] myIntArray = new int[2, 2] { { 1, 2 }, { 3, 4 } };
-//for (int i = 0; i < 2; i++)
-//{
-//    for (int j = 0; j < 2; j++)
+//    for (int j = 0; j < lengthOfSecondDimension; j++)
 //    {
-//        if (j % 2 == 1)
-//            Console.WriteLine(myIntArray[i, j]);
-//        else
-//            Console.Write(myIntArray[i, j] + " ");
+//        string inputString = Console.ReadLine();
+//        my2dIntArray[i,j] = int.Parse(inputString);
 //    }
 //}
 
 
-int[,] my2dIntArray = new int[2, 3];
+//for (int i = 0; i < lengthOfFirstDimension; i++)
+//{
+//    for (int j = 0; j < lengthOfSecondDimension; j++)
+//    {
+//            Console.Write(my2dIntArray[i, j] + " ");
+//    }
+//}
 
-int lengthOfFirstDimension = my2dIntArray.GetLength(0);
-int lengthOfSecondDimension = my2dIntArray.GetLength(1);
+int[][] myJaggedArray = new int[2][]; //arichxubebs tu ertidaigive sigrdzis araa
 
-for (int i = 0; i < lengthOfFirstDimension; i++)
+myJaggedArray[0] = new int[2] { 1, 2 };
+myJaggedArray[1] = new int[1] { 1 };
+
+for (int i = 0; i < myJaggedArray.Length; i++)
 {
-    for (int j = 0; j < lengthOfSecondDimension; j++)
-    {
-        string inputString = Console.ReadLine();
-        my2dIntArray[i,j] = int.Parse(inputString);
-    }
-}
+    var lengthOfArray = myJaggedArray[i].Length;
 
-
-for (int i = 0; i < lengthOfFirstDimension; i++)
-{
-    for (int j = 0; j < lengthOfSecondDimension; j++)
+    for (int j = 0; j < lengthOfArray; j++)
     {
-            Console.Write(my2dIntArray[i, j] + " ");
+        Console.WriteLine(myJaggedArray[i][j]);
     }
 }
