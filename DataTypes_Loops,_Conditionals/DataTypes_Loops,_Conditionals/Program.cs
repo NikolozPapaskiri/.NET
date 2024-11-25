3d4195d2325e3caba78ae41281bce42812835421
Nikoloz Papaskiri <nkpapaskiri@gmail.com>
2024-11-25 21:22:42 +0400

    Insertion sort

--- DataTypes_Loops,_Conditionals/DataTypes_Loops,_Conditionals/Program.cs
+++ DataTypes_Loops,_Conditionals/DataTypes_Loops,_Conditionals/Program.cs
@@ -158,6 +158,24 @@ foreach (int num in arr)
 Console.WriteLine("\nInsertion sort");
 arr = new int[] { 64, 34, 25, 12, 22, 11, 90 };
 
+for (int i = 1; i < n - 1 ; i++)
+{
+    temp = arr[i];
+    int j = i - 1;
+    // Move elements of arr[0..i-1], that are greater than temp, to one position ahead of their current position
+    while (j >= 0 && arr[j] > temp)
+    {
+        arr[j + 1] = arr[j];
+        j--;
+    }
+    arr[j + 1] = temp;
+}
+
+foreach (int num in arr)
+{
+    Console.Write(num + " ");
+}
+
 //Exercise 5: Temperature Converter
 
 //Exercise 6: Factorial Calculator
