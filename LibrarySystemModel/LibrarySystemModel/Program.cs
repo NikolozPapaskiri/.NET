1b6c0211f03a8679445b084e35bfa3dfb1dc5354
Nikoloz Papaskiri <nkpapaskiri@gmail.com>
2025-06-14 01:10:36 +0400

    added requirements

--- LibrarySystemModel/LibrarySystemModel/Program.cs
+++ LibrarySystemModel/LibrarySystemModel/Program.cs
@@ -2,17 +2,17 @@
 
 namespace LibrarySystemModel;
 
-/*
-Define a base class LibraryItem with properties like Title, Author, PublicationYear, and a virtual method DisplayDetails().
-Create derived classes Book (with an ISBN property) and DVD (with a DurationInMinutes property). Override DisplayDetails() in each derived class to include specific information.
-Create a LibraryMember class with properties MemberId, Name, and a method BorrowItem(LibraryItem item).
-Instantiate several Book and DVD objects, and a LibraryMember object. Demonstrate borrowing items and displaying their details. 
-*/
-
 public class Program
 {
     public static void Main(string[] args)
     {
+        /*
+        Define a base class LibraryItem with properties like Title, Author, PublicationYear, and a virtual method DisplayDetails().
+        Create derived classes Book (with an ISBN property) and DVD (with a DurationInMinutes property). Override DisplayDetails() in each derived class to include specific information.
+        Create a LibraryMember class with properties MemberId, Name, and a method BorrowItem(LibraryItem item).
+        Instantiate several Book and DVD objects, and a LibraryMember object. Demonstrate borrowing items and displaying their details. 
+        */
+
         // 1. Instantiate library items
         var book1 = new Book("Book1", "Niko", 2025, "978 - 0137517112");
         var book2 = new Book("Clean Code", "Robert C. Martin", 2008, "978-0132350884");
