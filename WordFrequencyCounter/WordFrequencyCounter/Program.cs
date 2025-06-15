98ac23d33e20c0e579cd29aafd1918eb0db1bd09
Nikoloz Papaskiri <nkpapaskiri@gmail.com>
2025-06-16 00:01:16 +0400

    WordFrequencyCounter

---
+++ WordFrequencyCounter/WordFrequencyCounter/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace WordFrequencyCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your text: ");
+
+            string strInput = Console.ReadLine().;
+
+            var result = WordFrequencyCounter(strInput);
+
+            foreach (var item in result) 
+            {
+                Console.WriteLine($"Word: {item.Key}, Frequency: {item.Value}");
+            }
+        }
+
+        public static Dictionary<string, int> WordFrequencyCounter(string inputStr)
+        {
+            string cleanText = inputStr.ToLower().Trim();
+
+            var wordFrequencies = new Dictionary<string, int>();
+
+            string[] list = cleanText.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string str in list)
+            {
+                if (wordFrequencies.ContainsKey(str))
+                {
+                    wordFrequencies[str]++;
+                }
+                else
+                {
+                    wordFrequencies.Add(str, 1);
+                }
+            }
+
+            return wordFrequencies;
+        }
+    }
+}
