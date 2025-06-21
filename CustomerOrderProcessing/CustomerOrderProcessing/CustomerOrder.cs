069e17e4f0ad2315318692f678edac052eae4f88
Nikoloz Papaskiri <nkpapaskiri@gmail.com>
2025-06-22 03:24:29 +0400

    CustomerOrderProcessing

---
+++ CustomerOrderProcessing/CustomerOrderProcessing/CustomerOrder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerOrderProcessing
+{
+    public class CustomerOrder
+    {
+        public int OrderId;
+        public DateTime OrderDate;
+
+        private static Queue<CustomerOrder> customerOrdersQueue = new Queue<CustomerOrder>();
+
+        public CustomerOrder(int orderId, DateTime orderDate)
+        {
+            OrderDate = orderDate;
+            OrderId = orderId;
+        }
+
+        public static void ReceiveOrder(CustomerOrder customerOrder)
+        {
+            customerOrdersQueue.Enqueue(customerOrder);
+        }
+
+        public static void ShowOrder()
+        {
+            Console.WriteLine("Customer Orders:");
+
+            foreach (var item in customerOrdersQueue)
+            {
+                Console.WriteLine($"orderId: {item.OrderId}, orderDate: {item.OrderDate}");
+            }
+        }
+
+        public static void DequeueOrders()
+        {
+            while (customerOrdersQueue.Count > 0)
+            {
+                var customerOrder = customerOrdersQueue.Dequeue();
+                Console.WriteLine($"orderId: {customerOrder.OrderId}, orderDate: {customerOrder.OrderDate} has being processed");
+            }
+        }
+    }
+}
---
+++ CustomerOrderProcessing/CustomerOrderProcessing/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CustomerOrderProcessing
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var customerOrder1 = new CustomerOrder(1, DateTime.Now);
+            var customerOrder2 = new CustomerOrder(2, DateTime.Now);
+
+            CustomerOrder.ReceiveOrder(customerOrder1);
+            CustomerOrder.ReceiveOrder(customerOrder2);
+
+            Console.WriteLine("___Show All The Customer Orders____");
+            CustomerOrder.ShowOrder();
+
+            Console.WriteLine("___Dequeue Customer Orders____");
+            CustomerOrder.DequeueOrders();
+
+            Console.WriteLine("___Show All The Customer Orders After Dequeue____");
+            CustomerOrder.ShowOrder();
+        }
+    }
+}
