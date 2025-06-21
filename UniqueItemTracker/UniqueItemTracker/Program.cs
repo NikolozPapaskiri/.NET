790ff748e65b331eb61bbf358804cab13fed8b71
Nikoloz Papaskiri <nkpapaskiri@gmail.com>
2025-06-22 03:35:36 +0400

    Unique Item Tracker

---
+++ UniqueItemTracker/UniqueItemTracker/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UniqueItemTrackerWordFrequencyCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] productIds = { "1", "2", "3", "2" };
+            HashSet<string> products = new HashSet<string>();
+
+            foreach (string productId in productIds)
+                products.Add(productId);
+
+                Console.WriteLine("Original Array List");
+            foreach (string productId in productIds)
+            {
+                Console.WriteLine(productId);
+            }
+
+            Console.WriteLine("\nUniqueue List");
+            foreach (string productId in products)
+                Console.WriteLine(productId);
+
+            
+        }
+    }
+}
